a95a81f861beaa4cd067f42aa8b375662ac04e38
Matheus Caputo <matheuscaputo17@gmail.com>
2020-02-25 13:26:51 -0300

    Created front of memory card

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 const $root = document.querySelector("#root");
 const $memoryCard = document.createElement("article");
+const $memoryCardFront = document.createElement("article");
+
 const $icon = `<img 
     class='icon' 
     src='img/icon-collabcode.svg' 
     alt='Gueio mascot' 
   />`;
 
-$memoryCard.classList.add("memory-card");
+const $iconJS = `<img 
+    class="icon" 
+    src="img/-icon-js.png" 
+    alt="JavaScript book icon"
+  />`;
 
+$memoryCard.classList.add("memory-card");
 $root.insertBefore($memoryCard, null);
 
+$memoryCardFront.classList.add("memory-card");
+$memoryCardFront.classList.add("-front");
+$root.insertBefore($memoryCardFront, $memoryCard);
+
 $memoryCard.insertAdjacentHTML("afterbegin", $icon);
+$memoryCardFront.insertAdjacentHTML("afterbegin", $iconJS);
