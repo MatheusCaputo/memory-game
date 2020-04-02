4599034aba035574bb63660e52aef6449470197e
Matheus Caputo <matheuscaputo17@gmail.com>
2020-04-02 19:11:47 -0300

    Added the click event on all cards (Unique challenge -> lesson 23)

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -6,3 +6,11 @@ createMemoryCard();
 createMemoryCard();
 createMemoryCard();
 createMemoryCard();
+
+const $memoryCards = document.querySelectorAll(".memory-card");
+
+$memoryCards.forEach(function($memoryCard, key) {
+  $memoryCard.addEventListener("click", function($memoryCard) {
+    alert(`Click na carta ${key}`);
+  });
+});
