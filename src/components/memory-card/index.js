fea4fa489b28832cfc40b89603fa49a3c6516db0
Matheus Caputo <matheuscaputo17@gmail.com>
2020-06-04 15:08:59 -0300

    CSS injected via JavaScript using IIFE to create only one Style tag on head (challenge-> lesson 26)

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -10,3 +10,48 @@ const createMemoryCard = ({ className, src, alt }) => `
 `;
 
 const handleClick = () => alert("cartinha");
+
+(() => {
+  const $head = document.querySelector("head");
+
+  const $style = document.createElement("style");
+
+  $style.textContent = `
+  .memory-card {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #f25a70;
+    height: 155px;
+    width: 155px;
+    border-radius: 30px;
+    box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+    position: relative;
+    cursor: pointer;
+  }
+
+  .memory-card.-front {
+    background-color: #fff;
+  }
+
+  .memory-card.-front::before {
+    content: "";
+    width: 95px;
+    height: 95px;
+    background-color: #d4d4d4;
+    border-radius: 50%;
+    position: absolute;
+  }
+
+  .memory-card > .icon {
+    width: 100px;
+    height: 100px;
+  }
+
+  .memory-card.-front > .icon {
+    position: absolute;
+    transform: translateY(-13px);
+  }`;
+
+  $head.insertBefore($style, null);
+})();
